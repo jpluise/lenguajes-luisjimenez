86dbb1ae3343b5e334bcb860fea3f1aa3424158f
T-107 <T-107@PC250>
2015-09-14 19:27:05 -0500

    listo!!

---
+++ src/luis.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author T-107
+ */
+public class luis {
+
+    
+    public static void main(String [] args){
+        
+         System.out.println("mi primer programa java"); 
+    }
+  
+
+}
