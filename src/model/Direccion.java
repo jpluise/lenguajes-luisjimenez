d69a4d0b8b3e17ed1fecdd687aac0e1fb60da94d
T-107 <T-107@PC251>
2015-10-01 19:46:37 -0500

    clase de jueves 1/10/15

---
+++ src/model/Direccion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author T-107
+ */
+public class Direccion { //Se agrego public, se ve en todos los paquetes
+    
+    String calle;
+    String municipio;
+    String colonia;
+    
+    
+}
