9697e78ef322e02b101393986e25992b145085fd
T-107 <T-107@PC251>
2015-11-30 19:42:17 -0600

    clase de lunes 30/11/2015

---
+++ src/proyectofinal/EjercicioTipoCeneval.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.*;
+
+/**
+ *
+ * @author T-107
+ */
+public class EjercicioTipoCeneval  {
+    
+    /*
+    DESARROLLAR EL SIGUIENTE PROGRAMA
+    CREAR UNA VENTANA JFRAME CON JLABEL QUE AL 
+    EJECUTARSE APARESCA EN EL LABEL UN CONTADOR QUE INICIE DESDE 30
+    Y QUE HAGA UN CONTEO EN RERTROCESO. AL LLEGAR AL MUNERO 20,
+    SE DEBE CERRAR ESA VENTANA CON ORTO JLABEL QUE CONTENGA EL CONTADOR
+    DESDE EL 19 HACIA ATRAS. AL LLEGAR AL 10 DEBE CERRARSE DICHA VENTANA
+    Y DEBE APARECER UNA TERCERA JFRAME IGUAL QUE LAS ANTERIORES EL JLABEL DEBE
+    DE CONTINUAR AL CONTEO HASTA LLEGAR EL CERO Y ALLI DEBE QUEDARSE DICHA VENTANA
+    CON EL CONTADOR CERO
+    EL DECREMENTO EN TODO EL CONTEO ES DE 1 SEGUNDO.
+    */
+ 
+}
